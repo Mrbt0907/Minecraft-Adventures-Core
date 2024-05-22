2486eeb64d05c663ad5b337476f2ba3168454b07
Mrbt0907 <Mrbt0907@MrBTPC1>
2024-05-21 17:33:15 -0700

    Undid attribute change to support mod compatability

--- src/main/java/net/endermanofdoom/mac/item/ItemSwordEX.java
+++ src/main/java/net/endermanofdoom/mac/item/ItemSwordEX.java
@@ -1,6 +1,5 @@
 package net.endermanofdoom.mac.item;
 
-import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 
 import net.endermanofdoom.mac.interfaces.ISetBonus;
@@ -29,7 +28,8 @@ public abstract class ItemSwordEX extends ItemSword implements ISetBonus
 	@Override
 	public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot equipmentSlot)
     {
-		Multimap<String, AttributeModifier> modifiers = HashMultimap.<String, AttributeModifier>create();
+		Multimap<String, AttributeModifier> modifiers = super.getItemAttributeModifiers(equipmentSlot);
+		
 		if (equipmentSlot == EntityEquipmentSlot.MAINHAND)
 			modifiers.put(MACAttributes.ATTACK_RANGE.getName(), new AttributeModifier(MACAttributes.ATTACK_RANGE_UUID, "Weapon modifier", attackReach, 0));
 		return modifiers;
