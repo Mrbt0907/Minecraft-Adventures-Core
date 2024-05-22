a895394b2e335ade6164724087373430f11b374b
gnang <gnang@Ender-puter>
2024-05-21 20:05:43 -0400

    special

--- src/main/java/net/endermanofdoom/mac/item/ItemSwordEX.java
+++ src/main/java/net/endermanofdoom/mac/item/ItemSwordEX.java
@@ -1,5 +1,6 @@
 package net.endermanofdoom.mac.item;
 
+import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 
 import net.endermanofdoom.mac.interfaces.ISetBonus;
@@ -28,7 +29,7 @@ public abstract class ItemSwordEX extends ItemSword implements ISetBonus
 	@Override
 	public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot equipmentSlot)
     {
-		Multimap<String, AttributeModifier> modifiers = super.getItemAttributeModifiers(equipmentSlot);
+		Multimap<String, AttributeModifier> modifiers = HashMultimap.<String, AttributeModifier>create();
 		if (equipmentSlot == EntityEquipmentSlot.MAINHAND)
 			modifiers.put(MACAttributes.ATTACK_RANGE.getName(), new AttributeModifier(MACAttributes.ATTACK_RANGE_UUID, "Weapon modifier", attackReach, 0));
 		return modifiers;
